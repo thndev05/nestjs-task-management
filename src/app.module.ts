837676c94ee34cd7a38cbf03668133a5a817b00c
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2025-09-28 10:25:08 +0700

    feat: implement authentication module with user entity, service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { TasksModule } from './tasks/tasks.module';
       }),
     }),
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
