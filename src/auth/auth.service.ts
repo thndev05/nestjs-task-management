8c7b1f36345d11709cfe63baeac1f33af06e9786
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2025-10-01 18:32:30 +0700

    feat: implement user signup functionality with password hashing and validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -10,5 +12,26 @@ export class AuthService {
     private userRepository: Repository<User>
   ) {}
 
-  
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({ 
+      username, 
+      password: hashedPassword 
+    });
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
