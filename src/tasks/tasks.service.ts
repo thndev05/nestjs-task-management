160687a17aab4c62784e312c5e4e943e6b52d967
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2025-10-10 21:42:59 +0700

    feat: update getTasks method to filter tasks by user

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,10 +14,12 @@ export class TasksService {
     private taskRepository: Repository<Task>,
   ) {}
 
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
 
     const query = this.taskRepository.createQueryBuilder('task');
+
+    query.andWhere({ user: user });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
@@ -25,7 +27,7 @@ export class TasksService {
 
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` }
       )
     }
